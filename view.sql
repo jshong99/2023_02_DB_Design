43c99cc74fe4749dc24ef70574b62e87b2fa6e9e
jshong99 <143462975+jshong99@users.noreply.github.com>
2024-06-25 14:18:43 +0900

    Delete view.sql

--- view.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW `DB_2023_Term_Proj`.`login` AS
-SELECT id, password, 'Doctor' AS role FROM `DB_2023_Term_Proj`.`Doctor`
-UNION ALL
-SELECT id, password, 'Nurse' AS role FROM `DB_2023_Term_Proj`.`Nurse`
-UNION ALL
-SELECT id, password, 'Patient' AS role FROM `DB_2023_Term_Proj`.`Patient`
-UNION ALL
-SELECT id, password, 'Admin' AS role FROM `DB_2023_Term_Proj`.`Admin`;
